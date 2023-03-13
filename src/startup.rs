2c85db0c6bccbbbcb4e154f3d6b52bfa6faa9962
Piotr Makowiecki <pmakowiecki0@gmail.com>
2023-03-13 21:28:43 +0100

    feat: add admin dashboard

--- src/startup.rs
+++ src/startup.rs
@@ -14,7 +14,7 @@ use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
 
 use crate::routes::{
-    confirm, health_check, home, login, login_form, publish_newsletter, subscribe,
+    admin_dashboard, confirm, health_check, home, login, login_form, publish_newsletter, subscribe,
 };
 
 pub fn get_connection_pool(config: &DatabaseSettings) -> PgPool {
@@ -108,6 +108,7 @@ pub async fn run(
             .route("/subscriptions", web::post().to(subscribe))
             .route("/subscriptions/confirm", web::get().to(confirm))
             .route("/newsletters", web::post().to(publish_newsletter))
+            .route("/admin/dashboard", web::get().to(admin_dashboard))
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())

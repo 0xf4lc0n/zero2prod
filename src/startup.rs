d2bf7385c06c56d95ef1f075df774f4ea6392a05
Piotr Makowiecki <pmakowiecki0@gmail.com>
2023-03-29 21:36:24 +0200

    feat: add password reset for admin

--- src/startup.rs
+++ src/startup.rs
@@ -14,7 +14,8 @@ use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
 
 use crate::routes::{
-    admin_dashboard, confirm, health_check, home, login, login_form, publish_newsletter, subscribe,
+    admin_dashboard, change_password, change_password_form, confirm, health_check, home, login,
+    login_form, publish_newsletter, subscribe,
 };
 
 pub fn get_connection_pool(config: &DatabaseSettings) -> PgPool {
@@ -109,6 +110,8 @@ pub async fn run(
             .route("/subscriptions/confirm", web::get().to(confirm))
             .route("/newsletters", web::post().to(publish_newsletter))
             .route("/admin/dashboard", web::get().to(admin_dashboard))
+            .route("/admin/password", web::get().to(change_password_form))
+            .route("/admin/password", web::post().to(change_password))
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
